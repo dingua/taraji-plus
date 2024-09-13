6c106c626fc7d5f2b03a3a3d942e1376f44a3011
Mejdi Lassidi <mejdi.lassidi@shopify.com>
2024-09-13 18:00:29 -0400

    Revert "add login page"
    This reverts commit f795e28a61a361b353fbcb583ab17917b0a43864.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { useSession } from 'next-auth/react';
 import HomePage from './HomePage/HomePage';
 import ArticlePage from './ArticlePage/ArticlePage';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Login from './components/Login';
-import TarajiTVPage from './TarajiTVPage/TarajiTVPage';
 import './App.css';
+import TarajiTVPage from './TarajiTVPage/TarajiTVPage';
 
 const App: React.FC = () => {
   const [competitionTypeId, setCompetitionTypeId] = useState<number>(0);
-  const { data: session } = useSession();
 
   const handleFilterChange = (id: number) => {
     setCompetitionTypeId(id);
@@ -22,19 +19,12 @@ const App: React.FC = () => {
       <div className="App">
         <Header onFilterChange={handleFilterChange} currentFilter={competitionTypeId} />
         <main>
-          {!session ? (
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="*" element={<Navigate to="/login" replace />} />
-            </Routes>
-          ) : (
             <Routes>
               <Route path="/" element={<Navigate to="/articles" replace />} />
               <Route path="/articles" element={<HomePage competitionTypeId={competitionTypeId} />} />
               <Route path="/article/:id" element={<ArticlePage />} />
               <Route path="/taraji-tv" element={<TarajiTVPage />} />
             </Routes>
-          )}
         </main>
         <Footer />
       </div>
