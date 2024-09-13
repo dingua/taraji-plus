58e7454da282c1b928584779b43749308a199342
Mejdi Lassidi <mejdi.lassidi@shopify.com>
2024-09-13 18:07:50 -0400

    make featured article clickable

--- src/components/FeaturedArticle.tsx
+++ src/components/FeaturedArticle.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Article } from '../types';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
 interface Props {
   article: Article;
@@ -7,9 +9,11 @@ interface Props {
 
 const FeaturedArticle: React.FC<Props> = ({ article }) => (
   <section className="featured-article">
+    <Link to={`/article/${article.article_id}`} className="link-unstyled">
     <img src={article.article_image_path} alt={article.article_title} />
     <h2>{article.article_title}</h2>
     <p>{article.category_label}</p>
+    </Link>
   </section>
 );
 
